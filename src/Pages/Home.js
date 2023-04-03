870a39387717a923a87019e27ed20f41f77209c0
Poorneshcoder <eswar.chinni354@gmail.com>
2023-04-03 17:08:42 +0530

    first_router_commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () =>{
+    return(
+        <div>
+            <h1>Router App</h1>
+            <h3>Home</h3>
+        </div>
+    )
+}
+
+export default Home;
